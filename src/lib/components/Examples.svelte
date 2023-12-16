b394491099f88da35f11c58dbafbe670730527a1
Willian Pinheiro <willpinhaaa@gmail.com>
2023-12-15 21:14:42 -0300

    Add simple navbar with user dropdown

--- src/lib/components/Examples.svelte
+++ src/lib/components/Examples.svelte
@@ -6,8 +6,10 @@
 
 <div class="flex flex-col w-full gap-4">
     {#each selectedComponents as c}
-        <div class="p-6 rounded-box border border-neutral">
-            <svelte:component this={c} />
+        <div class="p-4 rounded-box border border-neutral">
+            <div class="shadow-lg">
+                <svelte:component this={c} />
+            </div>
         </div>
     {/each}
 </div>
---
+++ src/lib/examples/navbar/SimpleWithUserDropdown.svelte
@@ -0,0 +1,43 @@
+<div class="navbar justify-between items-center bg-base-300">
+    <!-- Logo -->
+    <img alt="DaisyCatalog" src="/logo.svg" class="w-12" />
+
+    <div class="dropdown dropdown-bottom dropdown-end">
+        <!-- Dropdown action -->
+        <button class="btn btn-link p-0">
+            <img alt="User" src="/user.png" class="w-10 hover:brightness-90" />
+        </button>
+
+        <!-- Dropdown content -->
+        <ul
+            class="dropdown-content z-[1] menu p-2 shadow bg-base-200 rounded-box w-52"
+        >
+            <!-- User name and email -->
+            <div class="flex flex-col cap-2 p-2 border-b border-b-neutral">
+                <span class="text-xl font-bold line-clamp-1 break-all">
+                    Some guy with long name
+                </span>
+
+                <span class="text-sm text-secondary line-clamp-1 break-all">
+                    some.guy@with.very.long.email
+                </span>
+            </div>
+
+            <!-- Menu items -->
+
+            <li>
+                <a href="/">
+                    <i class="fa-solid fa-user"></i>
+                    Profile
+                </a>
+            </li>
+
+            <li>
+                <a href="/">
+                    <i class="fa-solid fa-right-from-bracket"></i>
+                    Logout
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
