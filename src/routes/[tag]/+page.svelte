173e3b8592e338580ad7a6ed37eaf6781a6fc995
Willian Pinheiro <willpinhaaa@gmail.com>
2023-12-17 18:13:37 -0300

    Add heading to tag route

---
+++ src/routes/[tag]/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	export let data;
+
+	const { tag } = data;
+</script>
+
+<div class="flex gap-2 border-b border-b-neutral">
+	<a href="/" class="btn btn-ghost px-2">
+		<i class="fa-solid fa-arrow-left-long text-3xl"></i>
+	</a>
+
+	<h1 class="text-4xl font-bold">#{tag}</h1>
+</div>
