7de31f47185de940afe8e33f0743c9fab415bf97
Willian Pinheiro <willpinhaaa@gmail.com>
2023-12-17 21:22:01 -0300

    Add mock imports

--- src/lib/examples/index.js
+++ src/lib/examples/index.js
@@ -1,3 +1,11 @@
-export default [
-    import("./navbar/WithUserDropdown.svelte")
-];
+export default {
+    "navbar": [
+        import("./navbar/Navbar1.svelte"),
+        import("./navbar/Navbar2.svelte"),
+    ],
+    "blog": [
+        import("./blog/Blog1.svelte"),
+        import("./blog/Blog2.svelte"),
+        import("./blog/Blog3.svelte"),
+    ]
+};
