fb59b18f5da2c37eb28bfb11f26380632110acdd
Willian Pinheiro <willpinhaaa@gmail.com>
2023-12-28 14:18:07 -0300

    Change hr bg color

--- src/routes/[tag]/+page.svelte
+++ src/routes/[tag]/+page.svelte
@@ -22,7 +22,7 @@
 	{:then examples}
 		<ul class="timeline timeline-snap-icon timeline-compact timeline-vertical">
 			{#each examples as example}
-				<Preview {example} />
+				<Preview {example} {tag} />
 			{/each}
 		</ul>
 	{/await}
