bac935aa04f7e7cc93b73c5b3b96b6c29d420f63
Willian Pinheiro <willpinhaaa@gmail.com>
2024-01-03 21:15:42 -0300

    implement tag examples and playground

---
+++ src/components/Preview.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	export let tag: string;
+	export let example: string;
+    export let html: string;
+
+	// Lower camel case to lower case with spaces
+	function convertExampleName(name: string): string {
+		return name.replace(/([a-z])([A-Z])/g, '$1 $2').toLowerCase();
+	}
+</script>
+
+<li>
+	<div class="timeline-middle flex">
+		<i class="fa-regular fa-folder-open"></i>
+	</div>
+
+	<div class="timeline-start w-full flex flex-col gap-4">
+		<span class="text-accent font-mono italic self-start">
+			{convertExampleName(example)}
+        </span>
+
+		<div role="tablist" class="tabs tabs-lifted pb-4">
+			<input
+				type="radio"
+				name="{example}"
+				role="tab"
+				class="tab"
+				aria-label="Preview"
+				checked
+			/>
+			<div role="tabpanel" class="tab-content border-base-300 rounded-box p-3">
+				{@html html}
+			</div>
+
+            <a href="https://github.com/willpinha/daisy-collection/blob/master/examples/{tag}/{example}.html" class="tab">
+                Code
+            </a>
+
+            <a href="/playground/{tag}/{example}" class="tab">Playground</a>
+		</div>
+	</div>
+
+	<hr class="bg-base-200"/>
+</li>
---
+++ src/components/TagExamples.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import { collection } from "../utils/collection";
+
+    import Preview from "../components/Preview.svelte";
+
+    export let tag: string;
+
+    const examples = collection[tag];
+</script>
+
+<div class="flex flex-col gap-8">
+    <div class="flex items-center gap-2">
+        <a href="/" class="btn btn-ghost px-3">
+            <span class="hidden">Get back</span>
+
+            <i class="fa-solid fa-arrow-left-long text-2xl"></i>
+        </a>
+
+        <h1 class="text-3xl font-bold text-primary">#{tag}</h1>
+    </div>
+
+    <ul class="timeline timeline-snap-icon timeline-compact timeline-vertical">
+        {#each Object.entries(examples) as [example, html]}
+            <Preview {tag} {example} {html} />
+        {/each}
+    </ul>
+</div>
