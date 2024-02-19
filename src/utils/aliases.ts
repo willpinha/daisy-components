1f12d02e6c68df716a1d319f708fb4a672de08d7
Willian Pinheiro <willpinhaaa@gmail.com>
2024-02-19 17:14:05 -0300

    35 add aliases for tag names (#91)
    * closes #35
    
    * rename heroSection tag to hero

---
+++ src/utils/aliases.ts
@@ -0,0 +1,99 @@
+const aliases: { [tag: string]: string[] } = {
+    "alert": [
+        "success",
+        "info",
+        "warning",
+        "error",
+        "danger",
+        "feedback",
+        "notification"
+    ],
+    "authentication": [
+        "login",
+        "register",
+        "account",
+        "signin",
+        "password",
+        "email"
+    ],
+    "card": [
+        "image",
+        "socials",
+        "product",
+        "stars"
+    ],
+    "cookies": [
+        "browser",
+        "web",
+        "internet",
+        "http",
+        "session"
+    ],
+    "dropdown": [
+        "list",
+        "select"
+    ],
+    "faq": [
+        "questions",
+        "q&a",
+        "support",
+        "help",
+        "assistance"
+    ],
+    "footer": [
+        "bottom",
+        "section",
+        "bar"
+    ],
+    "hero": [
+        "welcome",
+        "section",
+        "home",
+        "page"
+    ],
+    "input": [
+        "text",
+        "data",
+        "typing",
+        "form"
+    ],
+    "modal": [
+        "popup",
+        "dialog",
+        "overlay",
+        "window"
+    ],
+    "navbar": [
+        "section",
+        "menu",
+        "header",
+        "bar"
+    ],
+    "pricing": [
+        "money",
+        "costing",
+        "fee",
+        "charge"
+    ],
+    "sidebar": [
+        "bar",
+        "column",
+        "menu",
+        "section"
+    ],
+    "subscribe": [
+        "register",
+        "signup",
+        "follow",
+        "email",
+        "update"
+    ],
+    "team": [
+        "group",
+        "company",
+        "squad",
+        "employees"
+    ]
+};
+
+export default aliases;
