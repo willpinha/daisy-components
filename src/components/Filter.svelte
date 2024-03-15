4295bcf1223c75b171f1314890a02641139f385a
Willian Pinheiro <willpinhaaa@gmail.com>
2024-03-15 14:07:59 -0300

    Update text in Filter component

--- src/components/Filter.svelte
+++ src/components/Filter.svelte
@@ -25,13 +25,13 @@
 
 		<h1 class="text-4xl font-bold text-center">
 			Click on a <span class="text-primary">#tag</span> to show
-			<i class="text-secondary">UI examples</i>
+			<i class="text-secondary">components</i>
 		</h1>
 	</div>
 
 	<div class="text-center">
 		🌸 You can find
-		<span class="badge badge-accent badge-outline badge-sm">{numExamples}</span> UI examples in
+		<span class="badge badge-accent badge-outline badge-sm">{numExamples}</span> components in
 		<span class="badge badge-accent badge-outline badge-sm">{tags.length}</span> tags!
 	</div>
 
