1782c1d2e2ca9c3ebae3de102b05c2c1a72d073b
Willian Pinheiro <willpinhaaa@gmail.com>
2024-12-20 12:23:54 -0300

    change astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://components.willpinha.link",
+  site: "https://willpinha.github.io",
+  base: "/daisy-components/",
   integrations: [tailwind(), svelte()]
 });
